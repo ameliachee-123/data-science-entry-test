089a792adc2510804d93cfee20ff29643b9e67f5
ameliachee-123 <achx18@gmail.com>
2026-03-11 00:35:35 +0800

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,19 @@
+def swap(x, y):
+    """
+    Task 1
+    - Create a function that would swap the value of x and y using only x and y as variables.
+    - x and y must be numeric.
+    - Return -1 if x and y is not numeric, and
+    - print the swapped values if both x and y are numeric.
+    """
+
+    if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        return -1
+
+    x, y = y, x
+    print(x, y)
+
+
+# Task 2
+print(swap("Apple", 10))
+swap(9, 17)
